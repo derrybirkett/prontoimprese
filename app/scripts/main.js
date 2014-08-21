887dbda48d2cecd7064b954b7131b13cb5fd19e8
Derry Birkett <derry@d2tstudio.com>
2014-08-21 11:31:36 +0200

    Add Avanced Search to Homepage, with toggle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 /* Custom JS */
+
+
+$(document).ready(function() {
+
+    
+    /*
+
+    Usher
+    Classes with .usher class need a data-target and will open/close that target div
+
+    */
+
+    $(".usher").on("click", function() {
+        target = $(this).data("target");
+        $(target).slideToggle();
+    });
+
+});
